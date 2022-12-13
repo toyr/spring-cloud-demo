82d9b6175ea8615f3d9d409c8bfdcbc7ef3e62fd
谢高兵 <xiegaobing@ccbscf.com>
2022-12-13 14:10:00 +0800

    SecurityFilterChain

--- spring-boot-admin-server/src/main/java/com/example/springbootadminserver/config/SecurityConfiguration.java
+++ spring-boot-admin-server/src/main/java/com/example/springbootadminserver/config/SecurityConfiguration.java
@@ -1,6 +1,40 @@
-package com.example.springbootadminserver.config; /**
+package com.example.springbootadminserver.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+/**
  * @author xiegaobing
- * @description: 
+ * @description:
  * @date 2022/12/13 2:01 下午
- */public class SecurityConfiguration {
+ */
+@Configuration
+public class SecurityConfiguration {
+
+    private final String adminContextPath;
+
+    public SecurityConfiguration(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter( "redirectTo" );
+
+        http.authorizeRequests()
+                .antMatchers( adminContextPath + "/assets/**" ).permitAll()
+                .antMatchers( adminContextPath + "/login" ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage( adminContextPath + "/login" ).successHandler( successHandler ).and()
+                .logout().logoutUrl( adminContextPath + "/logout" ).and()
+                .httpBasic().and()
+                .csrf().disable();
+        return http.build();
+    }
 }
